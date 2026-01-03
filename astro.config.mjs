7117863a06889d972406f09d4c099ed00011334b
Samuel <sg1976387@gmail.com>
2026-01-03 09:14:41 -0300

    fix: Adjusted build settings

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,4 +6,8 @@ import sitemap from '@astrojs/sitemap';
 export default defineConfig({
 	site: 'https://croma-plum.vercel.app/',
 	integrations: [sitemap()],
+	build: {
+		format: 'file',
+		assets: 'assets',
+	},
 });
